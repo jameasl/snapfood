e6bff38d325186a1ab17bef5d4f7ec6147ff7a67
jameasl <s.jameasl@icdgroup.org>
2020-11-20 19:06:00 +0330

    add bootstrap rtl

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+document.getElementById('search').addEventListener('click', onSearchClick);
+
+function onSearchClick() {
+    const city = document.getElementById('city');
+    console.log(city.value)
+}
